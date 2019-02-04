0c4911a3d289120b07c512808aacbea9a7eb9f29
AbmaelFerreira <abmael_ninha@hotmail.com>
2019-02-03 23:44:12 -0300

    6 - Codificando o botão decremento

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ botaoIncrementa.addEventListener('click', incrementa)
 function incrementa(){
     input.value++
 }
-
 incrementa();
 
+var botaodecrementa = document.querySelector("#btn-decrementa");
+botaodecrementa.addEventListener('click', decrementa)
 
+
+function decrementa(){
+    input.value--
+}
+
+decrementa();
