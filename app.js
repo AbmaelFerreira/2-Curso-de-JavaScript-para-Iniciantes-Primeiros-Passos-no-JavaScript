48f44ed20df1c8b88cdace3fb99137bc2bd44b02
AbmaelFerreira <abmael_ninha@hotmail.com>
2019-02-14 01:06:59 -0300

    7 - FINALIZADO Calculando o total

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 var input = document.getElementById("quantidade");
 
+/*Implementação do Botão Incrementa*/
 var botaoIncrementa = document.querySelector("#btn-incrementa");
-
 botaoIncrementa.addEventListener('click', incrementa)
 
 function incrementa(){
-    input.value++
+    input.value++;
+
+    var item = botaoIncrementa.closest('.item');
+    var precoItem = item.querySelector('.preco-item');
+    var preco = Number(precoItem.textContent);
+
+    var elementoTotal = document.querySelector("#total");
+    elementoTotal.textContent = preco + Number(elementoTotal.textContent);
+
+
 }
 incrementa();
 
+
+
+
+
+/*Implementação do Botão Decrementa*/
 var botaodecrementa = document.querySelector("#btn-decrementa");
 botaodecrementa.addEventListener('click', decrementa)
-
 
 function decrementa(){
     input.value--
 }
-
 decrementa();
