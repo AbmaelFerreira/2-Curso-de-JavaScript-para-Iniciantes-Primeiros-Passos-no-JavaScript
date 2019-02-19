bdd0ba52fd3ef1e0a04a4f108684fcdb8cfd15bf
AbmaelFerreira <abmael_ninha@hotmail.com>
2019-02-18 23:40:20 -0400

    8 Aplicanco boas praticas com funções

--- app.js
+++ app.js
@@ -2,31 +2,46 @@ var input = document.getElementById("quantidade");
 
 /*Implementação do Botão Incrementa*/
 var botaoIncrementa = document.querySelector("#btn-incrementa");
-botaoIncrementa.addEventListener('click', incrementa)
+    botaoIncrementa.addEventListener('click', incrementa)
 
-function incrementa(){
-    input.value++;
-
-    var item = botaoIncrementa.closest('.item');
-    var precoItem = item.querySelector('.preco-item');
-    var preco = Number(precoItem.textContent);
-
-    var elementoTotal = document.querySelector("#total");
-    elementoTotal.textContent = preco + Number(elementoTotal.textContent);
-
-
-}
-incrementa();
-
-
-
-
-
+    
 /*Implementação do Botão Decrementa*/
 var botaodecrementa = document.querySelector("#btn-decrementa");
 botaodecrementa.addEventListener('click', decrementa)
 
+
+/*Funções do botão Incrementa */
+
+function incrementa(){
+    input.value++;
+    var item = botaoIncrementa.closest('.item');
+    var preco = pegaPrecoItem(item);
+    adicionaTotal(preco)
+  }
+
+
+/*Funções do botão Decrementa */
 function decrementa(){
-    input.value--
+    input.value--;
+    var item = botaodecrementa.closest('.item');
+
+    var preco = pegaPrecoItem(item);
+    subtraiTotal(preco)
 }
-decrementa();
+
+/*Funções Gerais */
+function pegaPrecoItem(item){
+    var precoItem = item.querySelector('.preco-item');
+    return Number(precoItem.textContent);
+}
+
+function adicionaTotal(valor){
+    var elementoTotal = document.querySelector("#total");
+    elementoTotal.textContent = valor + Number(elementoTotal.textContent);
+}
+
+function subtraiTotal(valor){
+    var elementoTotal = document.querySelector("#total");
+    elementoTotal.textContent = valor - Number(elementoTotal.textContent);
+}
+
